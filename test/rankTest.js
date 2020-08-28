3c43c23b7638078a458c04b2d060bb1840051e4b
xiaoyunlong <1758217010@qq.com>
2020-08-28 11:42:01 +0800

    [refactor-homework][lebron] test: add case2 voyageRisk test voyage length is 4

--- test/rankTest.js
+++ test/rankTest.js
@@ -19,3 +19,15 @@ rankTest('case1: voyageRisk test voyage length < 4', t => {
 
   t.is(result, 1);
 })
+
+rankTest('case2: voyageRisk test voyage length is 4', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 4,
+  };
+  const result = voyageRisk(voyage)
+
+  t.is(result, 1);
+})
+
+
