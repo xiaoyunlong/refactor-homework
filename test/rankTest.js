badacd5520b94d94a5400542ed058e44362926d4
xiaoyunlong <1758217010@qq.com>
2020-08-28 13:28:49 +0800

    [refactor-homework][lebron] test: add case19: voyageProfitFactor test history length < 10 and zone is west-indies and history.length > 8 and voyage.length = 14

--- test/rankTest.js
+++ test/rankTest.js
@@ -517,3 +517,53 @@ rankTest('case18: voyageProfitFactor test history length < 10 and zone is east-i
 
   t.is(result, 4);
 })
+
+
+rankTest('case19: voyageProfitFactor test history length < 10 and zone is west-indies  and history.length > 8 and voyage.length = 14', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 14,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },
+    {
+      zone: 'west-indies',
+      profit: 15,
+    },
+    {
+      zone: 'east-indies',
+      profit: 2,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+    {
+      zone: 'west-indies',
+      profit: 15,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+    {
+      zone: 'west-indies',
+      profit: 15,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    },
+    {
+      zone: 'west-africa',
+      profit: 7,
+    }
+  ];
+
+  const result = voyageProfitFactor(voyage, history)
+
+  t.is(result, 3);
+})
